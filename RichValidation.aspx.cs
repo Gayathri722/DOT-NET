ff03ab2c343d16405cb7b480b67a3cc354ec05dd
Gayathri722 <05gayu.r@gmail.com>
2026-03-09 15:06:02 +0530

    Add files via upload

---
+++ RichValidation.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+public partial class RichValidation : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        string name = txtName.Text;
+        string date = calDate.SelectedDate.ToShortDateString();
+
+        string fileName = "No file uploaded";
+
+        if (fileUpload1.HasFile)
+        {
+            fileName = fileUpload1.FileName;
+        }
+
+        lblOutput.Text = "Name: " + name +
+                         "<br/>Date: " + date +
+                         "<br/>File: " + fileName;
+    }
+}
